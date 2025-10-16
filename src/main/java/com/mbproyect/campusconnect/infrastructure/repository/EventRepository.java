a22b9b5c17dc67e09ff3e372c7e732bd562dcb04
juan <juanjuangmo@gmail.com>
2025-10-16 17:23:27 +0200

    Defined EventRepository with JPA interface

--- src/main/java/com/mbproyect/campusconnect/infrastructure/repository/EventRepository.java
+++ src/main/java/com/mbproyect/campusconnect/infrastructure/repository/EventRepository.java
@@ -1,11 +1,14 @@
-package com.juangomez.campusconnect.infrastructure.repository;
+package com.mbproyect.campusconnect.infrastructure.repository;
 
-import com.juangomez.campusconnect.model.entity.event.Event;
-import com.juangomez.campusconnect.model.entity.event.EventParticipant;
-import com.juangomez.campusconnect.model.enums.InterestTag;
+import com.mbproyect.campusconnect.model.entity.event.Event;
+import com.mbproyect.campusconnect.model.enums.EventStatus;
+import com.mbproyect.campusconnect.model.enums.InterestTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -13,9 +16,21 @@ import java.util.UUID;
 public interface EventRepository extends JpaRepository<Event, UUID> {
     Event getEventByEventId(UUID eventId);
 
-    Set<Event> findByEventTag(Set<InterestTag> eventTag);
-
-    Set<Event> findByDate(Date date);
-
     Set<Event> findByLocation_City(String locationCity);
+
+    @Query("SELECT e " +
+            "FROM Event e " +
+            "WHERE e.startDate >= :date AND e.eventStatus = :status " +
+            "ORDER BY e.startDate ASC")
+    List<Event> getUpcomingEvents (@Param("date") LocalDateTime date, @Param("status") EventStatus status);
+
+    @Query("""
+        SELECT e FROM Event e
+        JOIN e.eventBio b
+        JOIN b.interestTags t
+        WHERE t IN :tags 
+        AND e.eventStatus = :status
+    """)
+    Set<Event> getEventsByTags (@Param("tag") Set<InterestTag> tags, @Param("status") EventStatus status);
+
 }
