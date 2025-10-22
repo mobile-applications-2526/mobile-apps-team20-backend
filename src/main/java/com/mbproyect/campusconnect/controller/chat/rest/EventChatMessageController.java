82130deb11656a82d26f5ad52fd3c8e515bdfd02
juan <juanjuangmo@gmail.com>
2025-10-22 21:12:49 +0200

    Defined chatMessages controllers

--- src/main/java/com/mbproyect/campusconnect/controller/chat/rest/EventChatMessageController.java
+++ src/main/java/com/mbproyect/campusconnect/controller/chat/rest/EventChatMessageController.java
@@ -1,4 +1,7 @@
 package com.mbproyect.campusconnect.controller.chat.rest;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EventChatMessageController {
 }
